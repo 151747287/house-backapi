7e5a75a2c8886ad7709c953f2609545abac05d17
yudong <151747287@qq.com>
2020-06-06 15:51:05 +0800

    Initial commit

---
+++ src/main/java/com/team/house/housebackapi/mapper/HouseMapper.java
@@ -0,0 +1,25 @@
+package com.team.house.housebackapi.mapper;
+
+import com.team.house.housebackapi.entity.House;
+import com.team.house.housebackapi.entity.HouseExample;
+import java.util.List;
+
+public interface HouseMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(House record);
+
+    int insertSelective(House record);
+
+    List<House> selectByExample(HouseExample example);
+
+    House selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(House record);
+
+    int updateByPrimaryKey(House record);
+
+    //通过用户编号查询当前用户下的出租房
+    //修改House实体，添加类型名称tname、区域名称dname、街道名称sname
+    List<House> getHouseByUser(Integer id);
+}
